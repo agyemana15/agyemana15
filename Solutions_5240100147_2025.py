16e6ef8961c602915c6e011a4d5a5141742b89fd
agyemana15 <agyemana15@gmail.com>
2025-04-11 15:30:33 +0000

    Add files via upload
    solutions to assignment 3

---
+++ Solutions_5240100147_2025.py
@@ -0,0 +1,31 @@
+# 1. Reverse a string:
+
+text = "Programming"
+reversed_text = text[::-1] 
+print(reversed_text)
+
+# 2. Extract initials from a full name:
+
+full_name = input("Enter your full name: ")
+initials = ".".join([name[0].upper() for name in full_name.split()])
+print(initials + ".")
+
+# 3. Check if a string is a palindrome:
+
+text = input("Enter a word: ")
+if text == text[::-1]: 
+    print("It's a palindrome!")
+else:
+    print("It's not a palindrome.")
+
+# 4. Count words in a sentence:
+
+sentence = input("Enter a sentence: ")
+word_count = len(sentence.split())
+print(f"Word count: {word_count}")
+
+# 5. Replace occurrences of "is" with "was":
+
+text = "This is a string and it is an example."
+modified_text = text.replace("is", "was")
+print(modified_text)
